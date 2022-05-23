b4ab0a3be248c64b611dfc62cf015a0d24bd2aa2
Alice-Ross22 <aliciaflores@gmail.com>
2022-05-23 14:13:08 -0600

    entrada por tecldo de cadenas

---
+++ bin/main.dart
@@ -0,0 +1,22 @@
+import 'dart:io';
+
+import 'package:dart_application_5/dart_application_5.dart' as dart_application_5;
+
+void main(List<String> arguments) {
+  var nombre;
+  var direccion;
+  var sexo;
+
+  print("digite su nombre");
+  direccion= stdin.readLineSync();
+ 
+  print("digite su direccion");
+  nombre= stdin.readLineSync();
+
+   print("digite su sexo");
+  sexo= stdin.readLineSync();
+  sexo= stdin.readLineSync();
+
+ print("digite su sexo");
+  
+}
